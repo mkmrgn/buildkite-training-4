aadceea413ef0447e78f35e903ed684f77225e2a
mkmrgn <mike@morgan.ms>
2022-04-22 09:36:04 -0700

    fixed typo

--- .buildkite/tests.sh
+++ .buildkite/tests.sh
@@ -7,7 +7,7 @@ parallel_count=$BUILDKITE_PARALLEL_JOB_COUNT
 parallel_index=$BUILDKITE_PARALLEL_JOB
 tests_per_agent=$(expr test_count/parallel_count)
 agent_test_start=$(( (parallel_index*tests_per_agent)+1 ))
-agent_test_finish=$(( tests_per_agent*parallel_index ))
+agent_test_finish=$(( tests_per_agent*(parallel_index+1) ))
 
 echo "Start test $agent_test_start"
 echo "Finish test $agent_test_finish"
